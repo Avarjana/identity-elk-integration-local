0b67dde7044c5036379729a98cf95bf91c778b2d
Avarjana <acpasavarjana@gmail.com>
2022-03-29 14:30:19 +0530

    filebeat

---
+++ filebeat/filebeat.sh
@@ -0,0 +1,6 @@
+#cp filebeat.yml /etc/filebeat/filebeat.yml
+
+#chown -R elasticsearch /etc/filebeat
+#chown -R elasticsearch /usr/share/filebeat
+
+sudo /usr/share/filebeat/bin/filebeat --strict.perms=false -e -d "*"
--- logstash/logstash.sh
+++ logstash/logstash.sh
@@ -1,7 +1,26 @@
 #logstash
-mkdir -p /etc/logstash/config/certs
+config="http"
+while getopts c: flag
+do
+    case "${flag}" in
+        c) config=${OPTARG};;
+    esac
+done
 
-cp logstash.conf /usr/share/logstash/logstash-config.conf
+mkdir -p /etc/logstash/config/certs
+cp logstash.yml /etc/logstash/logstash.yml
+
+if [ $config = "filebeat" ]; then
+    echo "RUNNING IN FILEBEAT MODE"
+    cp filbeat.conf /usr/share/logstash/logstash-config.conf
+elif [ $config = "debug" ]; then
+    echo "RUNNING IN DEBUG MODE"
+    cp debug.conf /usr/share/logstash/logstash-config.conf
+else
+    echo "RUNNING IN HTTP MODE"
+    cp logstash.conf /usr/share/logstash/logstash-config.conf
+fi
+
 cp /etc/elasticsearch/certs/http_ca.crt /etc/logstash/config/certs/ca.crt
 
-sudo /usr/share/logstash/bin/logstash -f logstash-config.conf
+sudo /usr/share/logstash/bin/logstash -f logstash-config.conf --config.reload.automatic
