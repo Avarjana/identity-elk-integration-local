3eab74bb35f9827e6c43909263c2a7432bf96fbe
Avarjana <acpasavarjana@gmail.com>
2022-03-31 06:37:54 +0530

    OIDC1 config change

--- elasticsearch/elasticsearch.sh
+++ elasticsearch/elasticsearch.sh
@@ -13,12 +13,9 @@ cp jvm.options /etc/elasticsearch/jvm.options
 cp jswkset.json /etc/elasticsearch/jswkset.json
 
 # add metadata for SSO
-cp metadata_new.xml /etc/elasticsearch/config/saml/metadata_new.xml
+# cp metadata_new.xml /etc/elasticsearch/config/saml/metadata_new.xml
 
-# add client sercret for SSO
-# 5.11 UHY8cLG0UlQtFGQEEIvdXHnRtQQa
-echo "6YojW0iifmVvnDafb7YdfsqwYAMa" | sudo -u elasticsearch /usr/share/elasticsearch/bin/elasticsearch-keystore add -x 'xpack.security.authc.realms.oidc.oidc1.rp.client_secret' -f
-echo "Zl2Q03cTGXqgat7ntm9MfuP4eToa" | sudo -u elasticsearch /usr/share/elasticsearch/bin/elasticsearch-keystore add -x 'xpack.security.authc.realms.oidc.oidc2.rp.client_secret' -f
+echo "Zl2Q03cTGXqgat7ntm9MfuP4eToa" | sudo -u elasticsearch /usr/share/elasticsearch/bin/elasticsearch-keystore add -x 'xpack.security.authc.realms.oidc.oidc1.rp.client_secret' -f
 
 /usr/share/elasticsearch/bin/elasticsearch-users useradd wso2user -p changeme -r superuser
 
